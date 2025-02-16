3cf105045f558adf1016451c788cbf1e88dbc0ee
Ding Yangchen <dyc@DingdeMacBook-Pro.local>
2025-02-16 15:04:17 -0500

    add difference comparison

--- Readfiles.py
+++ Readfiles.py
@@ -4,6 +4,7 @@ import os
 import re
 from itertools import combinations
 import shutil
+import numpy as np
 
 class S_parameter:
     def __init__(self, path:Path, software:str):
@@ -41,13 +42,13 @@ class compare:
                (self.standard.network.f == self.comparison.network.f).all()):
                 Warning(f"The frequency of {self.standard.software} and {self.comparison.software} are not the same")
                 try:
-                    self.comparison.network.interpolate(self.standard.network.f)
-                except:
-                    self.standard.network.interpolate(self.comparison.network.f)
+                    self.comparison.network = self.comparison.network.interpolate(self.standard.network.f)
+                except ValueError:
+                    self.standard.network = self.standard.network.interpolate(self.comparison.network.f)
         else:
             print(f"{self.comparison.software} is not exist: {key}")
     
-    def snp_s2p(self, key:str, result_path:Path):
+    def snp_s2p(self, key:str, result_path:Path, cmp_type:str):
         if not result_path.exists():
             os.makedirs(result_path)
             print(f"Directory '{result_path}' created.")
@@ -57,32 +58,72 @@ class compare:
         combos = list(combinations(range(self.compare_list[key][2]), 2))
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots(len(combos), 4, figsize=(5*4, 3*len(combos)))
+        fig1, ax1 = plt.subplots(len(combos), 4, figsize=(5*4, 3*len(combos)))
         for j, combo in enumerate(combos):
             # get the subnetwork for the given port combination when the other port is matched to 50 ohms
             standard_s2p = rf.subnetwork(self.standard.network, combo)
             comparison_s2p = rf.subnetwork(self.comparison.network, combo)
             # plot the S-parameters
             for i, permutation in enumerate([(0, 0), (0, 1), (1, 0), (1, 1)]):
-                ax[j][i].set_title(f"S{combo[permutation[0]]+1}{combo[permutation[1]]+1}")
-                ax[j][i].set_xlabel("Frequency (GHz)")
-                ax[j][i].set_ylabel("S-parameter")
-                ax[j][i].grid(True)
-                standard_s2p.plot_s_db(m=permutation[0], n=permutation[1], ax=ax[j][i], label=f"{key}{self.standard.software}")
-                comparison_s2p.plot_s_db(m=permutation[0], n=permutation[1], ax=ax[j][i], label=f"{key}{self.comparison.software}")
-                ax[j][i].legend()
+                self.plot_s_parameters(key, ax, i, j, standard_s2p, comparison_s2p, combo, permutation)
+                self.compare_s2p(ax1, i, j, standard_s2p, comparison_s2p, combo, permutation, cmp_type)
         (result_path /"Figures").mkdir(parents=True, exist_ok=True)
         plt.tight_layout()
-        fig.savefig(result_path /"Figures"/f"{key}.png")
+        fig.savefig(result_path /"Figures"/f"{key}_S-parameters.png")
+        fig1.savefig(result_path /"Figures"/f"{key}_{cmp_type}_diff.png")
         plt.close(fig)
+    
+    def compare_s2p(self, ax, i, j, standard_s2p, comparison_s2p, combo, permutation, cmp_type):
+        frequency = standard_s2p.frequency.f
+        if cmp_type == 'abs':
+            standard = abs(standard_s2p.s[:, permutation[0], permutation[1]])
+            comparison = abs(comparison_s2p.s[:, permutation[0], permutation[1]])
+            # calculate the relative difference
+            relative_difference  = abs(standard - comparison) / standard * 100
+            # plot the difference
+            ax[j][i].plot(frequency, relative_difference, label='Difference')
+            ax[j][i].set_title(f"Abs Relative Difference S{combo[permutation[0]]+1}{combo[permutation[1]]+1}")
 
-    def compare_all(self, result_path = Path("./Results")):
+        elif cmp_type == 'db':
+            standard = 20*np.log10(np.abs(standard_s2p.s[:, permutation[0], permutation[1]]))
+            comparison = 20*np.log10(np.abs(comparison_s2p.s[:, permutation[0], permutation[1]]))
+            # calculate the relative difference
+            relative_difference  = abs(standard - comparison) / abs(standard) * 100
+            # plot the difference
+            ax[j][i].plot(frequency, relative_difference, label='Difference')
+            ax[j][i].set_title(f"dB Relative Difference S{combo[permutation[0]]+1}{combo[permutation[1]]+1}")
+        elif cmp_type == 'complex':
+            standard = standard_s2p.s[:, permutation[0], permutation[1]]
+            comparison = comparison_s2p.s[:, permutation[0], permutation[1]]
+            # calculate the relative difference
+            relative_difference  = ((standard.real-comparison.real)**2 + (standard.imag-comparison.imag)**2)**0.5 / abs(standard) * 100
+            # plot the difference
+            ax[j][i].plot(frequency, relative_difference, label='Difference')
+            ax[j][i].set_title(f"Complex Relative Difference S{combo[permutation[0]]+1}{combo[permutation[1]]+1}")
+        else:
+            raise ValueError('cmp_type must be abs, db or complex')
+        ax[j][i].plot(frequency, 5*np.ones_like(frequency), 'r--', label='5% Difference') 
+        ax[j][i].set_xlabel("Frequency (GHz)")
+        ax[j][i].set_ylabel("Difference (%)")
+        ax[j][i].legend()
+
+    def plot_s_parameters(self, key, ax, i, j, standard_s2p, comparison_s2p, combo, permutation):
+        ax[j][i].set_title(f"S{combo[permutation[0]]+1}{combo[permutation[1]]+1}")
+        ax[j][i].set_xlabel("Frequency (GHz)")
+        ax[j][i].set_ylabel("S-parameter")
+        ax[j][i].grid(True)
+        standard_s2p.plot_s_db(m=permutation[0], n=permutation[1], ax=ax[j][i], label=f"{key}{self.standard.software}")
+        comparison_s2p.plot_s_db(m=permutation[0], n=permutation[1], ax=ax[j][i], label=f"{key}{self.comparison.software}")
+        ax[j][i].legend()
+
+    def compare_all(self, result_path = Path("./Results"), cmp_type = 'db'):
         for key in self.compare_list:
             self.read(key)
-            self.snp_s2p(key, result_path)
+            self.snp_s2p(key, result_path, cmp_type)
 
 if __name__ == "__main__":
     compare_list = {}
     cst = S_parameter(Path("./Standard"), "_cst")
     tidy3d = S_parameter(Path("./Comparison"), "_tidy3d")
     cmp = compare(cst, tidy3d, compare_list)
-    cmp.compare_all()
+    cmp.compare_all(cmp_type='abs')
